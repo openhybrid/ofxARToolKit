2afb59115721aeac4187298dded5d423e8c21dcc
Valentin Heun <heun@mit.edu>
2016-02-06 18:38:53 -0500

    Empty Example and addon src files

---
+++ src/ofxARToolKit.h
@@ -0,0 +1,9 @@
+//
+//  ofxARToolKit.h
+//  emptyExample
+//
+//
+
+#pragma once
+
+#import "ofMain.h"
---
+++ src/ofxARToolKit.mm
@@ -0,0 +1,11 @@
+//
+//  ofxQCAR.cpp
+//  emptyExample
+//
+//
+
+#import "ofxARToolKit.h"
+
+#if !(TARGET_IPHONE_SIMULATOR)
+
+#import "ofxiOSExtras.h"
